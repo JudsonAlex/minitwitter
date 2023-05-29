c87f827b2b272807eeae8a76cfbd6cdcee2982e5
Judson <pequenojudosn@gmail.com>
2023-05-28 21:43:57 -0300

    django 3.2.19

--- minitwitter/settings.py
+++ minitwitter/settings.py
@@ -66,7 +66,7 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
