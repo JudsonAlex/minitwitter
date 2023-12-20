4c674a6b934d9daebfac0c0b4700d81fc5e951cb
Judson <52610794+JudsonAlex@users.noreply.github.com>
2023-12-19 21:19:49 -0300

    Update settings.py

--- minitwitter/settings.py
+++ minitwitter/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'minitwitter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASE_URL = os.environ.get('DATABASE_URL', default='postgresql://postgres:88736033@localhost:5432/minitwitter')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 DATABASES = {
     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
